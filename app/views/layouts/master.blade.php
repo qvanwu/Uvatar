57d97988dfb0b343a0028ed734d58b780d342377
Xiaofan Wu <qvanwu@gmail.com>
2015-03-17 12:18:03 +0100

    add js/css, add home.blade

---
+++ app/views/layouts/master.blade.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <link rel="stylesheet" href="{{ URL::asset('assets/css/bootstrap.min.css') }}"/>
+        <script src="{{ URL::asset('assets/js/bootstrap.min.js') }}"></script>
+    </head>
+
+    <body>
+        <nav class="navbar navbar-default">
+            <div class="container-fluid">
+                <div class="navbar-header">
+                    <a class="navbar-brand" href="#">Gravatar</a>
+                </div>
+            </div>
+        </nav>
+
+    @yield('body.content')
+
+    </body>
+</html>
