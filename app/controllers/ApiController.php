bdc2e8ab27f1f4a64dc8555d0d8724ddf6c5c005
Xiaofan Wu <qvanwu@gmail.com>
2015-03-29 18:18:45 +0200

    api

--- app/controllers/ApiController.php
+++ app/controllers/ApiController.php
@@ -3,39 +3,58 @@
 
 # NOTHING MODIFIED HERE
 
-class ApiController extends Controller {
+class ApiController extends Controller
+{
 
+    /**
+     * @return show api page
+     */
     public function show()
     {
         return View::make('api');
     }
 
     /**
+     * response a image with params
+     *
      * @param $md5 hashed email address
      * @param null $size image size
      * @param null $format image format
      * @return mixed
      */
 
-    public function getImage($md5, $size = '250px', $format = null)
+    public function getImage($md5, $size = 250, $format = 'jpg')
     {
         $account = Email::where('md5', $md5)->first();
-        $path = url('userimage') .'/'. $account->user_id . '/' . $account->main_avatar;
-        $image = HTML::image($path,                     // image path
-                            $account->email,            // image alt
-                            array('width' => $size,     // image size
-                                  'height' => $size));
+        $path = public_path() . '/userimage' . '/' . $account->user_id . '/' . $account->main_avatar;
 
-        return $image;
+        $image = Image::make($path);
+        return $image->resize($size, $size)->response($format);
     }
 
+    /**
+     * generate a url to fetch image
+     * @return mixed
+     */
     public function generate()
     {
-//        $email = Input::get('email');
-        $email = md5( strtolower( trim(Input::get('email'))));
-        $path = url('image').'/';
-        $result = '<img src="' . $path . $email.'"/>';
-        return Redirect::back()->with('result', $result);
+        $email = md5(strtolower(trim(Input::get('email'))));
+        $format = Input::get('format');
+        $size = Input::get('size');
+        $path = url('image') . '/';
+
+        if (is_null($format)){
+            $format = 'jpg';
+        }
+
+        if ($size == '') {
+            $size = 250;
+        }
+
+        $result = $path . $email . '/' . $size . '/' . $format;
+
+        return Redirect::back()->with(array('result' => $result,
+                                            'message' => 'Now you are able to get you image from the url generate.'));
     }
 
 }
--- app/views/api.blade.php
+++ app/views/api.blade.php
@@ -7,21 +7,37 @@
 @endsection
 
 <h4><em>Your avatars could be generated simply by using your address.
-Follow theses steps and use for your personal project.</em></h4>
+        Follow theses steps and use for your personal project.</em></h4>
 <hr/>
 
 
 <div class="row">
     <h5>1. We use a md5 hashed Email address to access your account.
-        So first pleas enter your Email address here</h5>
+        So first pleas enter your Email address here:</h5>
 
     {{Form::open(array('action' => 'ApiController@generate', 'method' => 'get'))}}
     <input type="email" name="email" placeholder="john.smith@oula.com" class="form-control"/>
     <br/>
-    {{Form::submit('Hash your Email', array('class' => 'btn btn-primary text-center'))}}
+
+    <h5>2. (Optional) Set image size: (pixel)</h5>
+
+    <div class="col-md-2">
+        <input type="text" name="size" placeholder="250" size="5" class="form-control" value=""/>
+    </div>
+    <br/><br/>
+
+    <h5>3. (Optional) Choose your preferred output format:</h5>
+
+    <label class="radio-inline"><input type="radio" name="format" value="jpg">jpeg</label>
+    <label class="radio-inline"><input type="radio" name="format" value="png">png</label>
+    <label class="radio-inline"><input type="radio" name="format" value="bmp">bmp</label>
+    <br/><br/><br/>
+
+    {{Form::submit('See result', array('class' => 'btn btn-primary text-center'))}}
     {{Form::close()}}
     <label for="">Copy the result to use</label>
-    <input type="text" value='{{Session::get("result")}}' disabled class="form-control"/>
+    <input type="text" value='{{Session::get("result")}}' readonly class="form-control"/>
+    <h4>{{Session::get('message')}}</h4>
 </div>
 
 @endsection
